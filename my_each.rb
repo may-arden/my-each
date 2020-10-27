7f54e0491ed987dd5e3a3110b24956b655e84a45
may arden <mayard3n@gmail.com>
2020-10-26 18:08:36 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,33 @@
-def my_each # put argument(s) here
-  # code here
+
+# define a method that uses yield and a while loop to pass each element of a collection
+# to a block.
+# gain a deeper understanding of the common iterator #each. 
+
+
+# collection = [1, 2, 3, 4]
+
+# my_each(collection) do |i|
+#   puts i
+# end 
+
+
+# def my_each # put argument(s) here
+#   # code here
+# end
+
+
+# def my_each # put argument(s) here
+#   # code here
+# end
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i += 1
+  end     
+  collection 
 end
+
+
+
